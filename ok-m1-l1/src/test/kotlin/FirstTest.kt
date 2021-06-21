f50e2cbf4b9c95efe2e99f7e9deb662876124c70
DarkSide <romangaponov@gmail.com>
2021-06-21 14:28:15 +0300

    first commit and task

---
+++ ok-m1-l1/src/test/kotlin/FirstTest.kt
@@ -0,0 +1,9 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FirstTest {
+    @Test
+    fun test(){
+       assertEquals(1+1,2)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "marketplace"
+include("ok-m1-l1")
+include("ok-m1-l1")
