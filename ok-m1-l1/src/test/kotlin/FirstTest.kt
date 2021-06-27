7b645bd7d834d6b4a5ecccd24fa45b5421506ef2
DarkSide <romangaponov@gmail.com>
2021-06-27 12:25:02 +0300

    first commit and task

--- ok-m1-l1/src/test/kotlin/FirstTest.kt
+++ ok-m1-l1/src/test/kotlin/FirstTest.kt
@@ -1,3 +1,4 @@
+import java.math.BigInteger
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -5,5 +6,28 @@ class FirstTest {
     @Test
     fun test(){
        assertEquals(1+1,2)
+    }
+    @Test
+    fun test2(){
+        val first = BigInteger("5")
+        val second = BigInteger("6")
+        val expectedPair = Pair(first, second)
+
+        assertEquals(expectedPair,foo(5L,6L))
+        assertEquals(expectedPair,foo(5,6))
+        assertEquals(expectedPair,foo(first,second))
+
+
+    }
+    fun <T, V> foo(a: T, b: V): Pair<BigInteger, BigInteger> {
+        return Pair(convertToBigInteger(a), convertToBigInteger(b))
+    }
+    fun<T> convertToBigInteger(value: T):BigInteger{
+        return when(value){
+            is Long ->  value.toBigInteger()
+            is Int -> value.toBigInteger()
+            is BigInteger -> value
+            else -> throw RuntimeException("unexpected  class")
+        }
     }
 }
